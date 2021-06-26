dc32cb22b8c368ecc54a57d197b3856b8d4208af
JakubWsz <jakub.wszola96@gmail.com>
2021-06-26 14:56:33 +0200

    init

---
+++ src/main/java/pl/kuba/CarRentalCompany2Application.java
@@ -0,0 +1,13 @@
+package pl.kuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalCompany2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalCompany2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kuba/CarRentalCompany2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalCompany2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
