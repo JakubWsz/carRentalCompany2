e3639671c7c9c86824d1fb3dbd10fb42a191b3f6
JakubWsz <jakub.wszola96@gmail.com>
2021-06-28 15:42:28 +0200

    ReservationRepository and WorkerRepository with @Repository

--- src/main/java/pl/kuba/infrastructure/ReservationRepository.java
+++ src/main/java/pl/kuba/infrastructure/ReservationRepository.java
@@ -1,8 +1,10 @@
 package pl.kuba.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.kuba.entities.Reservation;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation ,Integer> {
 
 }
--- src/main/java/pl/kuba/infrastructure/WorkerRepository.java
+++ src/main/java/pl/kuba/infrastructure/WorkerRepository.java
@@ -1,7 +1,9 @@
 package pl.kuba.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.kuba.entities.Worker;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Integer> {
 }
