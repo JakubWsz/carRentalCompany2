5c390e0825ceb642fa730bd228b0109f45e0df17
JakubWsz <jakub.wszola96@gmail.com>
2021-07-11 09:24:11 +0200

    problem with test method openNewBranchShouldBeNotNull in RentalCompanyServiceTests

--- src/test/java/pl/kuba/api/RentalCompanyAPITest.java
+++ src/test/java/pl/kuba/api/RentalCompanyAPITest.java
@@ -1,7 +1,23 @@
 package pl.kuba.api;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import pl.kuba.infrastructure.BranchRepository;
+import pl.kuba.infrastructure.RentalCompanyRepository;
 
 class RentalCompanyAPITest {
+    private final BranchRepository branchRepository;
+    private final RentalCompanyRepository rentalCompanyRepository;
+    private final RentalCompanyAPI rentalCompanyAPI;
 
+    RentalCompanyAPITest(BranchRepository branchRepository,
+                         RentalCompanyRepository rentalCompanyRepository, RentalCompanyAPI rentalCompanyAPI) {
+        this.branchRepository = branchRepository;
+        this.rentalCompanyRepository = rentalCompanyRepository;
+        this.rentalCompanyAPI = rentalCompanyAPI;
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
 }
